6921dfb4ed338902ffac7890a49031d3c0da2980
Helena Rosa <helena.rosa@aluno.senai.br>
2026-02-19 11:29:44 -0300

    fazendo o projeto do imc

---
+++ calculo-imc.js
@@ -0,0 +1,54 @@
+
+// obter o elemento
+// tratar os dados (converter, exibir os dados de forma correra )
+
+// variavel global
+let peso = 80;
+let altura = 1.75
+const imc = peso / (altura*altura);
+
+
+
+function classificaIMC(){   
+    const imc = peso / (altura*altura);
+if (imc < 18.5) {
+    console.log('Abaixo do peso');
+}else if (imc >= 18.5 && imc <25) {
+    console.log ('Peso normal'); 
+}else if (imc >= 25 && imc <30) {
+    console.log ('Sobrepeso');
+}else {
+    console.log('Obesidade');
+}
+};
+
+
+
+function calcularIMC(){
+console.log(`Este é o valor do IMC calculado: ${imc.toFixed(2)}.`);
+classificaIMC();
+};
+
+
+
+
+
+ 
+
+
+
+let classificacao
+
+
+
+
+
+
+
+function limparCampos(){
+
+};
+
+
+
+
---
+++ calculo-test-imc.js
@@ -0,0 +1,12 @@
+//captura global dos elementos
+
+const inputPeso = document.getElementById('Weight');
+const inputAltura = document.getElementById('height');
+const textResultado = document.getElementById('resultText');
+const formulario = document.getElementById('imcForm');
+
+
+
+function mostarResultado (mensagem) {
+    textoResultado.textContent = mensagem;
+}
